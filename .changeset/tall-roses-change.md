160c3a29ebf882868a3957570cf5df255b9e4042
lvisei <yunji.me@outlook.com>
2024-11-27 17:33:19 +0800

    feat: support table paraser (#37)
    * feat: support table paraser
    
    * Create tall-roses-change.md
    
    * chore: format

---
+++ .changeset/tall-roses-change.md
@@ -0,0 +1,5 @@
+---
+'@antv/gpt-vis': patch
+---
+
+feat: support table paraser
--- src/GPTVis/Lite.tsx
+++ src/GPTVis/Lite.tsx
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import type { Options } from 'react-markdown';
 import Markdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
 
 export interface GPTVisLiteProps extends Options {
   /** 自定义 markdown components样式 */
@@ -16,12 +17,14 @@ const GPTVisLite: React.FC<GPTVisLiteProps> = ({
   children,
   components,
   rehypePlugins,
+  remarkPlugins,
   ...rest
 }) => {
   return (
     <Markdown
       components={components}
       rehypePlugins={[rehypeRaw, ...(rehypePlugins ? rehypePlugins : [])]}
+      remarkPlugins={[remarkGfm, ...(remarkPlugins ? remarkPlugins : [])]}
       {...rest}
     >
       {children}
