7d8f0b1775d9baaf3251a81fdf98f6f85e391542
yunji <yunji.me@outlook.com>
2024-11-30 23:06:12 +0800

    wip: test

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,47 @@
 import type { Config } from 'jest';
+import { createJsWithTsPreset } from 'ts-jest';
 
+const compileModules = [
+  '@babel',
+  '.pnpm',
+  // Transform esm to cjs
+  'internmap',
+  'd3-*',
+  'lodash-es',
+];
+
+const ignoreList: string[] = [];
 const esm = ['internmap', 'd3-*'].join('|');
 
+// cnpm use `_` as prefix
+['', '_'].forEach((prefix) => {
+  compileModules.forEach((module) => {
+    ignoreList.push(`${prefix}${module}`);
+  });
+});
+
+const transformIgnorePatterns = [
+  // Ignore modules without es dir.
+  // Update: @babel/runtime should also be transformed
+  `<rootDir>/node_modules/(?!(${esm}))`,
+  `<rootDir>/node_modules/.pnpm/(?!(${esm}))`,
+  // `[/\\\\]node_modules[/\\\\](?!${ignoreList.join('|')})[^/\\\\]+?[/\\\\](?!(es)[/\\\\])`,
+];
+
 const config: Config = {
-  preset: 'ts-jest',
+  ...createJsWithTsPreset({
+    tsconfig: {
+      target: 'ESNext',
+      allowJs: true,
+    },
+  }),
   testEnvironment: 'jsdom',
   collectCoverage: false,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   testRegex: '(/__tests__/.*\\.(test|spec))\\.ts$',
   collectCoverageFrom: ['src/**/*.ts'],
   // Transform esm to cjs.
-  transformIgnorePatterns: [
-    `<rootDir>/node_modules/(?!(${esm}))`,
-    `<rootDir>/node_modules/.pnpm/(?!(${esm}))`,
-  ],
+  transformIgnorePatterns,
 };
 
 export default config;
